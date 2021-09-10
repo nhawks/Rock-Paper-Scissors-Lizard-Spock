b026784b971b2e898310080ec71bb7e9c79f2605
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-10 16:14:27 -0400

    created function to generate players based on game mode

--- player_list.py
+++ player_list.py
@@ -1,12 +1,19 @@
+from Ai import Ai
 from player_list import Player
 
 
 class PlayerList:
     def __init__(self):
-        players = []
+        self.players = []
 
 
-    def create_player(self):
+    def create_player(self, mode):
+        if mode == 1:
+         player_one = Player()
+         player_ai = Ai()
+         self.players = [player_one, player_ai]
+        
+        if mode == 2:
          player_one = Player()
          player_two = Player()
          self.players = [player_one, player_two]
