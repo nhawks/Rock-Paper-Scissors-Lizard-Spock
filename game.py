85704afdcd0e14c93ff5ae863674f726cf1fd70b
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-13 12:28:07 -0400

    replaced rules msg with variable imported from rules_art.py

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from win_conditions import WinConditions
+from rules_art import art
 
 class Game:
     def __init__(self) -> None:
@@ -23,20 +24,7 @@ class Game:
 
 
     def display_rules(self):
-        print(
-'''Welcome to: ROCK vs PAPER vs SCISSORS vs LIZARD vs SPOCK\n
-The rules are simple pick an item from the list at start of each round.
-Depending on your choice and your opponent's choice will decide who wins!
-You can play against AI or a friend and pick how many rounds you'd like to play.\n
-
-GESTURE BREAKDOWN\n
-________________________________________________________
-Rock crushes Scissors       | Scissors cuts Paper\n
-Paper covers Rock           | Rock crushes Lizard\n
-Lizard poisons Spock        | Spock smashes Scissors\n
-Scissors decapitates Lizard | Lizard eats Paper\n
-Paper disproves Spock       | Spock vaporizes Rock
-_________________________________________________________\n''')
+        print(art)
     
     def generate_players (self):
         self.Win_Condition.player_list.create_player(self.mode)
