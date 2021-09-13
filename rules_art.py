071921a85936e9a077a6ec884c7e97dac545836b
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-13 14:58:39 -0400

    minor changes to rules_art UI

--- rules_art.py
+++ rules_art.py
@@ -1,7 +1,7 @@
 rules_art = '''
 ____________________________________________________________________________
 | 🥊📃✂🦎🖖 | ROCK vs PAPER vs SCISSORS vs LIZARD vs SPOCK | 🥊📃✂🦎🖖     |
-| The rules are simple pick an item from the list at start of each round   |
+|  The rules are simple pick an item from the list at start of each round  |
 | Depending on your choice and your opponent's choice will decide who wins!|
 |                   You can play against AI or a friend                    |
 |                  Pick how many rounds you'd like to play.                |               
@@ -9,10 +9,15 @@ ____________________________________________________________________________
 |                            GESTURE BREAKDOWN                             |
 |__________________________________________________________________________|
 |__________________________________________________________________________|
-|            Rock crushes Scissors        |    Scissors cuts Paper         |       
-|            Paper covers Rock            |    Rock crushes Lizard         | 
-|            Lizard poisons Spock         |    Spock smashes Scissors      |    
-|            Scissors decapitates Lizard  |    Lizard eats Paper           |     
-|            Paper disproves Spock        |    Spock vaporizes Rock        |  
+|        Rock crushes Scissors         |       Scissors cuts Paper         |   
+|--------------------------------------|-----------------------------------|    
+|        Paper covers Rock             |       Rock crushes Lizard         | 
+|--------------------------------------|-----------------------------------| 
+|        Lizard poisons Spock          |    Spock smashes Scissors         |    
+|--------------------------------------|-----------------------------------| 
+|        Scissors decapitates Lizard   |    Lizard eats Paper              | 
+|--------------------------------------|-----------------------------------|      
+|        Paper disproves Spock         |    Spock vaporizes Rock           |   
+|--------------------------------------|-----------------------------------|    
 |__________________________________________________________________________|
 |__________________________________________________________________________|'''
